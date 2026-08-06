b1bcd8cb4dffa93475140b7585c00d3e313c14ba
Lazaro Toconas <lazaro.toconas19@gmail.com>
2026-08-06 16:44:17 -0400

    Added background

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Background from './background'
 
 function App() {
 
   return (
     <>
+      <Background />
       <section id="center">
         Lazaro Toconas building something cool with React and Vite
       </section>
---
+++ src/background/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+export default function Background() {
+  return (
+    <div className="bg-wrapper">
+      <div className="bg-blob bg-blob-1"></div>
+      <div className="bg-blob bg-blob-2"></div>
+      <div className="bg-blob bg-blob-3"></div>
+    </div>
+  );
+}
